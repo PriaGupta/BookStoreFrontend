aab657c7ce0ca3c642eb3bdf8e07647fdf63fc63
PriaGupta <guppriya02@gmail.com>
2024-07-23 17:59:55 +0530

    console backend url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import Banner from '../components/Banner'
 import RecentlyAdded from '../components/RecentlyAdded'
 
 const Home = () => {
+
+  console.log(process.env.backend_url,'backend_url');
+
+
   return (
     <div className='bg-zinc-800 text-white px-10 py-8'>
       <Banner/>
